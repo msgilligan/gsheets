fa08262bc4081ce97ae9fa670d7a44faaa99c734
Ken Krebs <kk@kktec.com>
2012-09-08 17:10:52 -0500

    Fixed codenarc violations in smoke tests; ignoring gradle gui settings

--- src/test/groovy/org/gsheets/ExcelFileSmokeTests.groovy
+++ src/test/groovy/org/gsheets/ExcelFileSmokeTests.groovy
@@ -21,8 +21,8 @@
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  DISCLAIMED. IN NO EVENT SHALL Andre Steingress BE LIABLE FOR ANY
  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES,
+ LOSS OF USE, DATA, OR PROFITS, OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -31,10 +31,9 @@
  */
 package org.gsheets
 
-import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.usermodel.CellStyle
 import org.apache.poi.ss.usermodel.Font
-import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.ss.usermodel.Workbook
 
 /**
  * Simple smoke tests for ExcelFile
@@ -46,40 +45,43 @@ class ExcelFileSmokeTests extends GroovyTestCase {
     File excel
 
     void setUp() {
-        excel = new File("test.xls")
-        if (!excel.exists()) excel.createNewFile()
+        excel = new File('test.xls')
+        if (!excel.exists()) {
+			 excel.createNewFile()
+        }
     }
 
     void testCreateSimpleWorkbook()  {
         Workbook workbook = new ExcelFile().workbook {
 
             styles {
-                font("bold")  { Font font ->
+                font('bold')  { Font font ->
                     font.setBoldweight(Font.BOLDWEIGHT_BOLD)
                 }
 
-                cellStyle ("header")  { CellStyle cellStyle ->
+                cellStyle ('header')  { CellStyle cellStyle ->
                     cellStyle.setAlignment(CellStyle.ALIGN_CENTER)
 
                 }
             }
 
             data {
-                // data
-                sheet ("Export")  {
-                    header(["Column1", "Column2", "Column3"])
+                sheet ('Export')  {
+                    header(['Column1', 'Column2', 'Column3'])
 
-                    row([10, 20, "=A2*B2"])
-                    row(["", "", "=sum(C2)"])
+                    row([10, 20, '=A2*B2'])
+                    row(['', '', '=sum(C2)'])
                 }
             }
 
             commands {
-                applyCellStyle(cellStyle: "header", font: "bold", rows: 1, columns: 1..3)
+                applyCellStyle(cellStyle: 'header', font: 'bold', rows: 1, columns: 1..3)
                 applyColumnWidth(columns: 1..2, width: 200)
                 // mergeCells(rows: 1, columns: 1..3)
             }
         }
+		
+		assert workbook.numberOfSheets == 1
 
         def excelOut = new FileOutputStream(excel)
         workbook.write(excelOut)
